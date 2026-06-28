47003ef680824be3b0f1871bd9b73f95abdcf0b5
Nikita Maghade <maghadenikita1429@gmail.com>
2026-06-28 12:39:04 +0530

    added navbar and footer to lead form

--- app/lead/page.tsx
+++ app/lead/page.tsx
@@ -1,9 +1,13 @@
+import Footer from "../components/Footer";
 import LeadForm from "../components/LeadForm";
+import Navbar from "../components/Navbar";
 
 export default function ParentingPage() {
   return (
     <>
+      <Navbar />
       <LeadForm />
+      <Footer />
     </>
   );
 }
